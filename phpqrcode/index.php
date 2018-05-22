cedc4c7d13934df8dfbba4a71a639cb5aa24582e
sasika de silva <sivasasika@gmail.com>
2018-05-23 01:40:36 +0530

    finalized

--- phpqrcode/index.php
+++ phpqrcode/index.php
@@ -37,7 +37,7 @@
         mkdir($PNG_TEMP_DIR);
     
     
-    $filename = $PNG_TEMP_DIR.'test.png';
+    $filename = $PNG_TEMP_DIR.'images.png';
     
     //processing form input
     //remember to sanitize user input in real-life solution !!!
@@ -57,7 +57,7 @@
             die('data cannot be empty! <a href="?">back</a>');
             
         // user data
-        $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
+        $filename = $PNG_TEMP_DIR.'images'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
         QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
         
     } else {    
